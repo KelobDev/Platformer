bb8e2bba598569287a87d70f3d21101747f16cd7
KelobKicjow <114358909+KelobDev@users.noreply.github.com>
2026-03-20 18:09:15 +0100

    Added wall jumps + polished collision system
    Dodałem wstępne wall jumpy oraz zmieniłem system kolizji na trochę lepszy do tego systemu

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+
 public class PlayerController : MonoBehaviour
 {
+    [Header("Layers")]
+    [SerializeField] private LayerMask groundLayer;
+
     [Header("InputSystem")]
     public PlayerInputAction playerControls;
 
@@ -9,6 +14,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0f, 100f)] private float maxSpeed = 4f;
     [SerializeField, Range(0f, 100f)] private float maxAcceleration = 35f;
     [SerializeField, Range(0f, 100f)] private float maxAirAcceleration = 20f;
+
+    [Header("Collisions")]
+    [SerializeField, Range(0f, 1f)] private float collisionRadius = 0.25f;
+    [SerializeField] private Vector2 bottomOffset, rightOffset, leftOffset;
+    private bool onGround, onWall;
 
 
     private  InputAction move;
@@ -25,13 +35,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0, 5)] private int maxAirJumps = 0;
     [SerializeField, Range(0f, 5f)] private float downwardMovementMultiplier = 3f;
     [SerializeField, Range(0f, 5f)] private float upwardMovementMultiplier = 1.7f;
-
     private InputAction jump;
     private int jumpPhase;
     private float defaultGravityScale;
 
+    [Header("Wall jumping")]
+    [SerializeField, Range(0f, 100f)] private float slideSpeed = 10;
+    [SerializeField, Range(0f, 100f)] private float wallJumpLerp = 10;
+    private bool onRightWall, onLeftWall;
+    private int wallSide;
+
+
     private bool desiredJump;
-    private bool onGround;
 
 
     private Rigidbody2D rb;
@@ -61,7 +76,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        desiredJump = jump.ReadValue<float>()!=0;
+        //checks if player is touching ground
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+        //checks if player is touching wall
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer)
+            || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+
+        desiredJump = jump.IsPressed();
         direction = move.ReadValue<Vector2>();
         desiredVelocity = new Vector2(direction.x, 0f) * maxSpeed;
     }
@@ -78,10 +101,20 @@ public class PlayerController : MonoBehaviour
         {
             jumpPhase = 0;
         }
+        if(onWall && !onGround)
+        {
+            if(velocity.x !=0)
+            {
+                
+                WallSlide();
+            }
+        }
         if (desiredJump)
         {
             desiredJump = false;
+            
             Jump();
+            WallJump();
         }
         if(rb.linearVelocity.y > 0)
         {
@@ -112,28 +145,41 @@ public class PlayerController : MonoBehaviour
             velocity.y += jumpSpeed;
         }
     }
-
-    //checking if player is on ground
-    private void OnCollisionStay2D(Collision2D collision)
+    //wall interaction
+    private void WallJump()
     {
-        EvaluateCollision(collision);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        EvaluateCollision(collision);
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        onGround = false;//player leaves ground
-    }
-    private void EvaluateCollision(Collision2D collision)
-    {
-        for(int i =0; i<collision.contactCount; i++)
+        if ((onWall && !onGround) || jumpPhase < maxAirJumps)
         {
-            Vector2 normal = collision.GetContact(i).normal;
-            onGround |= normal.y >= 0.9f;
+            int wallDir = onRightWall ? -1 : 1;
+            jumpPhase += 1;
+            float jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * jumpHeight);
+            if (velocity.y > 0f)
+            {
+                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0);
+            }
+            velocity.y += jumpSpeed;
+            velocity.x = wallDir * wallJumpLerp; 
+
         }
+
     }
+    private void WallSlide()
+    {
+        bool pushingWall = (velocity.x == 0 && onRightWall) || (velocity.x == 0 && onLeftWall);
+
+        float push = pushingWall ? 0 : velocity.x;
+        velocity = new Vector2(push, -slideSpeed);
+    }
+    //display gorund detect points
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+    }
+
 }
